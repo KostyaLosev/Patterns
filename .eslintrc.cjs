23e6b70d96ba89865c573cede07df83739ad846a
KostyaLosev <losevkanstantin@gmail.com>
2026-02-10 23:55:38 +0200

    asd

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,28 @@
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
 
-export default [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json',
-        sourceType: 'module'
-      }
-    },
-    plugins: {
-      '@typescript-eslint': tseslint
-    },
-    rules: {
-      'import/prefer-default-export': 'off',
-      'no-console': 'off'
-    }
-  }
-];
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+
+  plugins: ['@typescript-eslint', 'import'],
+
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+
+  rules: {
+    'linebreak-style': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,12 @@
-module.exports = {
+export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   extensionsToTreatAsEsm: ['.ts'],
-
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-
   transform: {
     '^.+\\.ts$': ['ts-jest', { useESM: true }],
   },
-};
+}
