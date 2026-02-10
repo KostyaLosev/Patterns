67856271c17275514de22a2b620c2579898e3e16
KostyaLosev <155112459+KostyaLosev@users.noreply.github.com>
2026-02-10 12:08:30 +0200

    Implement triangle and pyramid app with factories validators and tests

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      'import/prefer-default-export': 'off',
+      'no-console': 'off'
+    }
+  }
+];
